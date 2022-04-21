a01b00083390b2213692ad3ca8bfbfa8e0d9325f
b a <ab@wanwangzi.onmicrosoft.com>
2022-04-21 13:20:46 +0800

    添加项目文件。

---
+++ 80.3.5/80.3.5.cpp
@@ -0,0 +1,21 @@
+﻿#include<stdio.h>
+int main()
+{
+	int a, b, d{};
+	char m;
+	printf("输入符号:\n");
+	scanf_s("%c", &m);
+	printf("两个数:\n");
+	scanf_s("%d", &a);
+	scanf_s("%d", &b);
+	if (m == '+')
+	{printf("%d%c%d=%d", a, m, b, a + b);}
+	if (m == '-')
+	{printf("%d%c%d=%d", a, m, b, a - b);}
+	if (m == '*')
+	{printf("%d%c%d=%d", a, m, b, a * b);}
+	if (m == '/')
+	{printf("%d%c%d=%d", a, m, b, a / b);}
+	if (m == '%')
+	{printf("%d%c%d=%d", a, m, b, a % b);}
+}
